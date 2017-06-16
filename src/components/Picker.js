7f186d930338ac8c7e89be8262b7f65401e5f067
Marina Hoashi <marinahoashi@gmail.com>
2017-06-16 11:26:00 -0400

    Refactored to getClarifaiTags function

--- src/components/Picker.js
+++ src/components/Picker.js
@@ -7,6 +7,8 @@ import styles from '../style'
 import app from '../clarifai'
 import { setPhotoUrl, setPhotoBase64, setPhotoTags } from '../redux/photo'
 
+/* -----------------    COMPONENT    ------------------ */
+
 const Picker = ({ photo, setPhoto, setBase64, setTags }) => {
   let { photoUrl, photoBase64, tags } = photo
 
@@ -30,20 +32,7 @@ const Picker = ({ photo, setPhoto, setBase64, setTags }) => {
         ImageEditor.cropImage(fixedPhotoUrl, imageSize, (imageUri) => {
           ImageStore.getBase64ForTag(imageUri, (base64Data) => {
             setBase64(base64Data)
-
-            app.models.predict(Clarifai.FOOD_MODEL, { base64: base64Data })
-            .then((res) => {
-              console.log('Clarifai result = ', res);
-              let tags = [];
-              for (let i = 0; i < res.outputs[0].data.concepts.length; i++) {
-                tags.push(res.outputs[0].data.concepts[i].name)
-              }
-              setTags(tags)
-            },
-            (error)=>{
-              console.log(error);
-            });
-
+            getClarifaiTags(base64Data)
             ImageStore.removeImageForTag(imageUri);
           }, (reason) => console.log('ERROR 3: ', reason) )
         }, (reason) => console.log('ERROR 2: ', reason) )
@@ -51,10 +40,25 @@ const Picker = ({ photo, setPhoto, setBase64, setTags }) => {
     }
   };
 
+  const getClarifaiTags = (base64) => {
+    app.models.predict(Clarifai.FOOD_MODEL, { base64: base64 })
+    .then((res) => {
+      // console.log('Clarifai result = ', res);
+      let tags = []
+      const concepts = res.outputs[0].data.concepts
+      concepts.forEach(concept => {
+        tags.push(concept.name)
+      })
+      setTags(tags)
+    }, (error) => {
+      console.log(error);
+    })
+  }
+
   return (
     <View style={ styles.container }>
       <Button
-        title="Pick an image from camera roll"
+        title="Pick a food photo from your camera roll!"
         onPress={ pickImage }
       />
 
@@ -67,8 +71,7 @@ const Picker = ({ photo, setPhoto, setBase64, setTags }) => {
   )
 }
 
-
-/* -----------------    CONTAINER     ------------------ */
+/* -----------------    CONTAINER    ------------------ */
 
 const mapState = ({ photo }) => ({ photo })
 const mapDispatch = dispatch => ({
@@ -84,71 +87,3 @@ const mapDispatch = dispatch => ({
 })
 
 export default connect(mapState, mapDispatch)(Picker)
-
-// export default class Picker extends React.Component {
-//   state = {
-//     image: null,
-//     tagText: '',
-//     pictureBase64: ''
-//   };
-
-//   render() {
-//     let { image } = this.state;
-
-//     return (
-//       <View style={ styles.container }>
-//         <Button
-//           title="Pick an image from camera roll"
-//           onPress={ this._pickImage }
-//         />
-//         {image &&
-//           <Image source={{ uri: image }} style={ styles.image } />}
-
-//         <Text>{this.state.tagText}</Text>
-//       </View>
-//     );
-//   }
-
-  // _pickImage = async () => {
-  //   let result = await ImagePicker.launchImageLibraryAsync({
-  //     allowsEditing: true,
-  //     aspect: [4, 3],
-  //   });
-
-  //   if (!result.cancelled) {
-  //     this.setState({ image: result.uri.replace('file://', '') });
-
-  //     let imageURL = this.state.image;
-
-  //     Image.getSize(imageURL, (width, height) => {
-  //       let imageSize = {
-  //         size: { width, height },
-  //         offset: { x: 0, y: 0 }
-  //       }
-
-  //       // https://github.com/facebook/react-native/issues/12114
-  //       ImageEditor.cropImage(imageURL, imageSize, (imageURI) => {
-  //         ImageStore.getBase64ForTag(imageURI, (base64Data) => {
-  //           this.setState({ pictureBase64: base64Data });
-
-  //           let testBase64 = this.state.pictureBase64
-  //           app.models.predict(Clarifai.FOOD_MODEL, { base64: this.state.pictureBase64 })
-  //           .then((res) => {
-  //             // console.log('Clarifai result = ', res);
-  //             let tags = '';
-  //             for (let i = 0; i<res.outputs[0].data.concepts.length; i++) {
-  //               tags += res.outputs[0].data.concepts[i].name + ' ';
-  //             }
-  //             this.setState({ tagText: tags });
-  //           },
-  //           (error)=>{
-  //             console.log(error);
-  //           });
-
-  //           ImageStore.removeImageForTag(imageURI);
-  //         }, (reason) => console.log(reason) )
-  //       }, (reason) => console.log(reason) )
-  //     }, (reason) => console.log(reason))
-  //   }
-  // };
-// }
