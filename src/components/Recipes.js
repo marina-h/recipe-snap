bf6bdf05d3e16f456adb1cc51cf3f56e8d81cd11
Marina Hoashi <marinahoashi@gmail.com>
2017-06-16 17:00:34 -0400

    Change Recipes to smart container

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Button, Image, View, Text, ScrollView, Linking } from 'react-native'
 import { connect } from 'react-redux'
 
@@ -7,42 +7,49 @@ import styles from '../style'
 
 /* -----------------    COMPONENT    ------------------ */
 
-const Recipes = ({ photo, recipes }) => {
-  let { photoUrl, tags } = photo
-  let { recipeList } = recipes
+class Recipes extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: `Your Recipes`,
+  })
 
-  const visitRecipeUrl = (url) => {
-    Linking.openURL(url)
-    .catch(err => console.error('Error: ', err))
+  render() {
+    let { photoUrl, tags } = this.props.photo
+    let { recipeList } = this.props.recipes
+
+    const visitRecipeUrl = (url) => {
+      Linking.openURL(url)
+      .catch(err => console.error('Error: ', err))
+    }
+    return (
+      <View>
+        <ScrollView>
+
+          {/* Maybe add a separate screen to select ingredients from tags list */}
+          {/* Possibly make each recipe show more info (ingredients, calories) when tapped */}
+
+          { photoUrl
+          ? <Image source={{ uri: photoUrl }} style={ styles.image } />
+          : null }
+
+          <Text>Here are the ingredients I see: { tags.join(', ') }</Text>
+
+          {
+            recipeList.map((recipe, idx) => (
+              <View key={ idx } >
+                <Image source={{ uri: recipe.image }} style={ styles.image } />
+                <Text>{ recipe.label }</Text>
+                <Text>Source: { recipe.source }</Text>
+                <Button
+                  title="Link to original website"
+                  onPress={ () => visitRecipeUrl(recipe.url) } />
+              </View>
+            ))
+          }
+
+        </ScrollView>
+      </View>
+    )
   }
-
-  return (
-    <View>
-      <ScrollView>
-
-        {/* Maybe add a way to select ingredients from tags list */}
-
-        { photoUrl
-        ? <Image source={{ uri: photoUrl }} style={ styles.image } />
-        : null }
-
-        <Text>Here are the ingredients I see: { tags.join(' ') }</Text>
-
-        {
-          recipeList.map((recipe, idx) => (
-            <View key={ idx } >
-              <Image source={{ uri: recipe.image }} style={ styles.image } />
-              <Text>{ recipe.label }</Text>
-              <Button
-                title="Link to original website"
-                onPress={ () => visitRecipeUrl(recipe.url) } />
-            </View>
-          ))
-        }
-
-      </ScrollView>
-    </View>
-  )
 }
 
 /* -----------------   REACT-REDUX   ------------------ */
