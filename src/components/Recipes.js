918123852439c2392ac8d8aa2ab606449c6f2760
Marina Hoashi <marinahoashi@gmail.com>
2017-06-17 22:26:54 -0400

    Made recipes swipeable

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Button, Image, View, Text, ScrollView, Linking, Share, TouchableHighlight } from 'react-native'
 import { connect } from 'react-redux'
+import Swipeout from 'react-native-swipeout'
 
 import styles from '../style'
 
@@ -32,6 +33,16 @@ class Recipes extends Component {
       .catch(err => console.error('Error: ', err))
     }
 
+    const swipeoutBtns = [
+      {
+        text: 'Button',
+        onPress: () => {
+          console.log('Pressed')
+        },
+        sensitivity: 35
+      }
+    ]
+
     return (
       <View>
         <ScrollView>
@@ -46,27 +57,30 @@ class Recipes extends Component {
           <Text>Here are the ingredients I see: { tags.join(', ') }</Text>
           <Text>Here are your dietary preferences: { preferences.join(', ') }</Text>
 
-          {/*{ Show something if there are no recipes :( }*/}
           {
             recipeList.length
             ? recipeList.map((recipe, idx) => (
-              <View key={ idx } >
-                <Image source={{ uri: recipe.image }} style={ styles.image } />
-                <Text>{ recipe.label }</Text>
-                <Text>Source: { recipe.source }</Text>
-                <TouchableHighlight onPress={ () => this.shareRecipe(recipe.label, recipe.url) }>
-                  <View>
-                    <Text>Click to share this recipe</Text>
-                  </View>
-                </TouchableHighlight>
-                <Button
-                  title="Link to original website"
-                  onPress={ () => visitRecipeUrl(recipe.url) }
-                  />
+              <Swipeout key={ idx }
+                right={ swipeoutBtns }
+                autoClose={ true }>
+                <View>
+                  <Image source={{ uri: recipe.image }} style={ styles.image } />
+                  <Text>{ recipe.label }</Text>
+                  <Text>Source: { recipe.source }</Text>
+                  <TouchableHighlight onPress={ () => this.shareRecipe(recipe.label, recipe.url) }>
+                    <View>
+                      <Text>Click to share this recipe</Text>
+                    </View>
+                  </TouchableHighlight>
+                  <Button
+                    title="Link to original website"
+                    onPress={ () => visitRecipeUrl(recipe.url) }
+                    />
 
-                {/*{ Add a select option to keep recipe in store. At the bottom of the page, add a Export link that can send all recipe links at once }*/}
+                  {/*{ Add a select option to keep recipe in store. At the bottom of the page, add a Export link that can send all recipe links at once }*/}
 
-              </View>
+                </View>
+              </Swipeout>
               ))
             : <Text style={ styles.mainText }>Sorry, I couldn't find any recipies with those ingredients and options :(</Text>
           }
